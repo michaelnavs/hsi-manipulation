4eebb2f520bc71b9390be860f825224167cf3543
michaelnavs <michaelnavs@gmail.com>
2021-04-12 22:42:01 -0500

    create dataset of slope values and save to excel sheet

--- main.py
+++ main.py
@@ -5,53 +5,58 @@ This program will open a HSI file and display spectral signature of a pixel in
 wavelength range of 460-530
 """
 import sys
+from typing import List, Tuple
 from spectral import open_image, imshow, get_rgb, BandInfo
 from scipy.stats import linregress
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnchoredText
+import pandas as pd
 
 
-def main(row: int, col: int) -> None:
-    img = open_image("./images/first_data_2_12_2021.bil.hdr")
-
-    wavelength_range = (460, 530)  # store desired wavelength range values
-
-    pixel = img.read_pixel(row, col)  # get reflectance values
-    wavelengths = img.bands.centers  # get wavelengths
+def reduce_dataset(wavelengths: List, wavelength_range: Tuple) -> Tuple[List, List]:
     reduced_wavelengths = []  # store desired wavelength values
     # store index of wavelength values above to get respective reflectance value
-    pixel_index = []
+    pixel_indices = []
 
     # keep wavelength values and index in range 460-530
     for index, wavelength in enumerate(wavelengths):
         if wavelength_range[0] <= wavelength <= wavelength_range[1]:
-            pixel_index.append(index)
+            pixel_indices.append(index)
             reduced_wavelengths.append(wavelength)
 
-    # reduced pixel values to match the index of respective wavelength values
-    pixel = [value for index, value in enumerate(pixel) if index in pixel_index]
+    return reduced_wavelengths, pixel_indices
 
-    # get values for line of best fit
-    reg = linregress(reduced_wavelengths, pixel)
-    intercept = reg.intercept
-    slope = reg.slope
-    best_fit_y_vals = [intercept + slope * value for value in reduced_wavelengths]
 
-    # plot spectral signature
-    fig, ax = plt.subplots()
-    ax.plot(reduced_wavelengths, pixel)
-    ax.plot(reduced_wavelengths, best_fit_y_vals)
-    ax.set(xlabel="wavelengths", ylabel="reflectance", title=f"Pixel {row}x{col}")
-    ax.grid()
-    at = AnchoredText(
-        f"Slope: {slope}", prop=dict(size=15), frameon=True, loc="upper left"
-    )
-    at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
-    ax.add_artist(at)
-    fig.savefig("plot.png")
-    plt.show()
+def main() -> None:
+    img = open_image("./images/first_data_2_12_2021.bil.hdr")
+    rows, cols = img.shape[0], img.shape[1]
+    wavelengths = img.bands.centers  # get wavelengths
+    wavelength_range = (460, 530)  # store desired wavelength range values
+    reduced_wavelengths, pixel_indices = reduce_dataset(wavelengths, wavelength_range)
+    writer = pd.ExcelWriter("dataset.xlsx", engine="xlsxwriter")
+
+    slopes = []
+
+    for row in range(rows):
+        print(f"Working on row {row}")
+        row_slopes = []
+        for col in range(cols):
+            pixel = img.read_pixel(row, col)  # get reflectance values
+            # reduced pixel values to match the index of respective wavelength values
+            pixel = [
+                value for index, value in enumerate(pixel) if index in pixel_indices
+            ]
+            # calculate slope using scipy.stats.linregress
+            slope = linregress(reduced_wavelengths, pixel).slope
+            row_slopes.append(slope)
+        slopes.append(row_slopes)
+
+    slopes_df = pd.DataFrame(slopes)
+    slopes_df.to_excel(writer, sheet_name="data", index=False)
+
+    writer.save()
 
 
 if __name__ == "__main__":
-    main(int(sys.argv[1]), int(sys.argv[2]))
+    main()
